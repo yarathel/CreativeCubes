8af2057cb817d49e08b5d0037055a3c2a7cd06e4
Ashley Baltodano <ashley.baltodano05@gmail.com>
2026-06-09 09:51:22 -0600

    Implementation of button dimensions and location in (x, y)

---
+++ Main/Globals.cpp
@@ -0,0 +1,46 @@
+#include "Globals.h"
+
+AppState currentState = MENU;
+
+Boton2D botonJugar =
+{
+    355.0f,
+    420.0f,
+    250.0f,
+    150.0f
+};
+
+Boton2D botonConfig =
+{
+    355.0f,
+    280.0f,
+    250.0f,
+    160.0f
+};
+
+Boton2D botonCredits =
+{
+    355.0f,
+    150.0f,
+    250.0f,
+    150.0f
+};
+
+Boton2D botonBack =
+{
+    30.0f,
+    50.0f,
+    200.0f,
+    100.0f
+};
+
+std::vector<glm::vec3> posicionesCubos;
+
+bool Boton2D::estaPresionado(double mouseX, double mouseY)
+{
+    return (
+        mouseX >= x &&
+        mouseX <= x + ancho &&
+        mouseY >= y &&
+        mouseY <= y + alto
+    );
