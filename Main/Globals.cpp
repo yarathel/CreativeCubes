236625ac336007d9bd9969c2ed9778d5606f4603
Ashley Baltodano <ashley.baltodano05@gmail.com>
2026-06-19 15:39:48 -0600

    Modify button positions and add pause menu buttons

--- Main/Globals.cpp
+++ Main/Globals.cpp
@@ -2,10 +2,11 @@
 
 AppState currentState = MENU;
 
+// --- BOTONES DEL MENÚ PRINCIPAL ---
 Boton2D botonJugar =
 {
     355.0f,
-    330.0f,
+    300.0f,
     250.0f,
     150.0f
 };
@@ -13,7 +14,7 @@ Boton2D botonJugar =
 Boton2D botonConfig =
 {
     355.0f,
-    200.0f,
+    180.0f,
     250.0f,
     160.0f
 };
@@ -26,6 +27,14 @@ Boton2D botonCredits =
     150.0f
 };
 
+Boton2D botonSalir =
+{
+    340.0f,
+    -35.0f,  
+    280.0f,
+    180.0f  
+};
+
 Boton2D botonBack =
 {
     30.0f,
@@ -34,8 +43,41 @@ Boton2D botonBack =
     100.0f
 };
 
+// --- NUEVOS BOTONES DEL MENÚ DE PAUSA ---
+Boton2D botonResume =
+{
+    344.0f,
+    350.0f, 
+    290.0f,
+    260.0f
+};
+
+Boton2D botonRules =
+{
+    345.0f,
+    180.0f, 
+    290.0f,
+    260.0f
+};
+
+Boton2D botonMainMenu =
+{
+    350.0f,
+    10.0f, 
+    280.0f,
+    260.0f
+};
+
+Boton2D tituloPausa =
+{
+    350.0f,
+    460.0f,
+    280.0f,
+    260.0f
+};
 std::vector<glm::vec3> posicionesCubos;
 
+// --- DETECCIÓN DE CLICS EN LOS BOTONES ---
 bool Boton2D::estaPresionado(double mouseX, double mouseY)
 {
     return (
@@ -43,5 +85,5 @@ bool Boton2D::estaPresionado(double mouseX, double mouseY)
         mouseX <= x + ancho &&
         mouseY >= y &&
         mouseY <= y + alto
-    );
+        );
 }
