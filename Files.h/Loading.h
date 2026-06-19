ffed6a0f7bc58ac43fea51b88c21fd51a85e96b5
Ashley Baltodano <ashley.baltodano05@gmail.com>
2026-06-19 15:28:44 -0600

    Update Loanding.h

--- Files.h/Loading.h
+++ Files.h/Loading.h
@@ -6,7 +6,6 @@
 #include "Texture.h"
 
 namespace Loading {
-    // Renderiza el cubo animado, el texto LOADING y la barra de progreso en movimiento
     void Render(GLFWwindow* window, Shader& cubeShader, unsigned int cubeVAO, Texture& texCuboMundo,
         Shader& menuShader, unsigned int fondoVAO, Texture& texLoadingText, float duration);
 }
